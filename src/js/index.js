b12ffc1b95c6eec7b026ac614da266257f3176c2
Ola Assem <ola.assem@viget.com>
2020-07-31 01:06:05 +0200

    Builds initial recipe model data

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -11,6 +12,11 @@ import { elements, renderLoader, clearLoader } from './views/base';
  **/
 
 const state = {}
+
+
+/** 
+ * SEARCH CONTROLLER
+ **/
 
 const controlSearch = async () => {
     // 1. Gey query from view
@@ -49,27 +55,10 @@ elements.searchResPages.addEventListener('click', e => {
 });
 
 
+/** 
+ * RECIPE CONTROLLER
+ **/
 
-
-/*
-
-👉 This is how you use forkify-api instead of the food2fork API.
-
-In the Search.js file (as soon as you get there), just replace:
-
-const res = await axios(`${PROXY}http://food2fork.com/api/search?key=${KEY}&q=${this.query}`);
-with this:
-
-const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
-
-
-Then, in Recipe.js (as soon as you get there), please replace:
-
-const res = await axios(`${PROXY}http://food2fork.com/api/get?key=${KEY}&rId=${this.id}`);
-with this:
-
-const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
-
-
-👉 That's it, that's all you need to know. Again, make these changes as you go through the projects. For now, just keep following the videos. And now, have fun with the project 😘
-*/
+ const r = new Recipe(46956);
+ r.getRecipe();
+ console.log(r);
